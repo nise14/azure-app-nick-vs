31b78e1e760a60719539e74c862ccec9bd18aa2e
Nicolas <nise14@htomail.com>
2025-10-24 12:22:43 -0500

    Updated welcome Message

--- obj/Debug/net8.0/Azure-app-nick.AssemblyInfo.cs
+++ obj/Debug/net8.0/Azure-app-nick.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Azure-app-nick")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+421397fcd034ebc17235c2b95e8616f37eafd349")]
 [assembly: System.Reflection.AssemblyProductAttribute("Azure-app-nick")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Azure-app-nick")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
